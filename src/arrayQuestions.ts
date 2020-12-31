5ed475913718dbc2c91594a4e0232fc5d9f815f9
Paulo Luiz <paulo.luiz127@gmail.com>
2020-12-31 19:01:32 -0300

    Correção erros de português

--- src/arrayQuestions.ts
+++ src/arrayQuestions.ts
@@ -2,11 +2,11 @@ const array = [
   {
     "questionText": "1 - O que você pensa sobre o futuro?",
     "options": {
-      "alternativeA": "Vivo a minha vida passo a passo, sempre construindo o dia de amanhã.",
+      "alternativeA": "Vivo minha vida passo a passo, sempre construindo o dia de amanhã.",
       "alternativeB": "Deixo a vida me levar, não penso muito no futuro.",
-      "alternativeC": "Vivo um dia após o outro, lutando para chegar no mês seguinte.",
+      "alternativeC": "Vivo um dia após o outro, lutando para chegar ao mês seguinte.",
       "alternativeD": "Abro mão de coisas hoje sempre pensando na realização dos meus sonhos.",
-      "alternativeE": "Planejo todos os meus passos, tenho metas muito definidas para os próximos anos."
+      "alternativeE": "Planejo todos os meus passos. Tenho metas muito definidas para os próximos anos."
     }
   },
   {
@@ -14,79 +14,79 @@ const array = [
     "options": {
       "alternativeA": "Sou do tipo que guarda sempre um pouco, porque preciso de segurança para o amanhã.",
       "alternativeB": "Sou do tipo que gasta sem pensar muito, porque sempre consigo dar um jeito no final.",
-      "alternativeC": "Sou do tipo que não consegue fazer sobrar, porque todo o dinheiro que entra é pra pagar as contas do mês.",
-      "alternativeD": "Sou do tipo que acredita no futuro, procuro investir tudo que posso no meu projeto pessoal.",
+      "alternativeC": "Sou do tipo que não consegue fazer sobrar, porque todo o dinheiro que entra é para pagar as contas do mês.",
+      "alternativeD": "Sou do tipo que acredita no futuro. Procuro investir tudo que posso no meu projeto pessoal.",
       "alternativeE": "Sou do tipo que faz metas, invisto tudo o que posso hoje planejando o que vou colher no futuro."
     }
   },
   {
     "questionText": "3 - Como é a sua relação com o dinheiro?",
     "options": {
-      "alternativeA": "Dinheiro pra mim é transformação, cada real me ajuda a fazer as coisas acontecerem.",
-      "alternativeB": "Dinheiro pra mim é prazer, eu preciso dele pra fazer as coisas que eu gosto.",
-      "alternativeC": "Dinheiro pra mim é solução de problemas, me dá fôlego pra viver cada mês.",
-      "alternativeD": "Dinheiro pra mim é combustível para realizar meu grande sonho.",
-      "alternativeE": "Dinheiro pra mim é resultado do meu planejamento para acumular sempre mais."
+      "alternativeA": "Dinheiro para mim é transformação, cada real me ajuda a fazer as coisas acontecerem.",
+      "alternativeB": "Dinheiro para mim é prazer, eu preciso dele pra fazer as coisas que eu gosto.",
+      "alternativeC": "Dinheiro para mim é solução de problemas, me dá fôlego pra viver cada mês.",
+      "alternativeD": "Dinheiro para mim é combustível para realizar meu grande sonho.",
+      "alternativeE": "Dinheiro para mim é resultado do meu planejamento para acumular sempre mais."
     }
   },
   {
     "questionText": "4 - Você poupa dinheiro?",
     "options": {
-      "alternativeA": "Poupo sempre que sobra, mesmo que pequenas quantidades.",
-      "alternativeB": "Não me preocupo tanto em poupar, prefiro viver o presente.",
-      "alternativeC": "Não consigo poupar, os gastos consomem tudo o que eu ganho no mês.",
-      "alternativeD": "Não vou realizar meus sonhos com pequenos valores, preciso poupar um valor alto.",
-      "alternativeE": "Guardar dinheiro é um compromisso, já reservo uma parte assim que entra na conta."
+      "alternativeA": "Poupo sempre que sobra, mesmo que em pequenas quantidades.",
+      "alternativeB": "Não me preocupo tanto em poupar. Prefiro viver o presente.",
+      "alternativeC": "Não consigo poupar. Os gastos consomem tudo que eu ganho no mês.",
+      "alternativeD": "Não vou realizar meus sonhos com pequenos valores. Preciso poupar um valor alto.",
+      "alternativeE": "Guardar dinheiro é um compromisso. Reservo uma parte assim que entra na conta."
     }
   },
   {
     "questionText": "5 - Você faz investimentos?",
     "options": {
-      "alternativeA": "Eu faço investimento aos poucos e sempre, inclusive em bens imóveis e outros.",
-      "alternativeB": "Investimento não é pra mim, não consigo pensar nisso.",
-      "alternativeC": "Investimento é algo importante, mas nunca me sobra dinheiro pra fazer.",
-      "alternativeD": "Investimento é algo grande -  uma boa quantia de dinheiro para viabilizar meu projeto pessoal.",
+      "alternativeA": "Eu faço investimento aos poucos e sempre. Inclusive em bens imóveis e outros.",
+      "alternativeB": "Investimento não é para mim. Não consigo pensar nisso.",
+      "alternativeC": "Investimento é algo importante, mas nunca sobra dinheiro para fazer.",
+      "alternativeD": "Investimento é algo grande: uma boa quantia de dinheiro para viabilizar meu projeto pessoal.",
       "alternativeE": "Eu faço investimento bem planejado, com foco nos resultados de longo prazo."
     }
   },
   {
     "questionText": "6 - Como você se relaciona com o banco?",
     "options": {
-      "alternativeA": "O banco pra mim é como se fosse um porquinho, onde eu deixo seguro o meu dinheiro guardado. ",
+      "alternativeA": "O banco para mim é como se fosse um porquinho, onde eu deixo seguro o meu dinheiro guardado. ",
       "alternativeB": "O banco é brigado comigo, como se fossemos duas pessoas que não se combinam muito.",
-      "alternativeC": "O banco pra mim é um mal necessário, uso o mínimo possível pra resolver os problemas.",
-      "alternativeD": "O banco pra mim é distante, ainda não enxergou meu potencial.",
-      "alternativeE": "O banco ajuda a maximizar o meu dinheiro, para que eu possa atingir meus planos"
+      "alternativeC": "O banco para mim é um mal necessário, uso o mínimo possível para resolver os problemas.",
+      "alternativeD": "O banco para mim é distante: ainda não enxergou o meu potencial.",
+      "alternativeE": "O banco ajuda a maximizar o meu dinheiro para que eu possa atingir meus planos"
     }
   },
   {
     "questionText": "7 - Como você se relaciona com a família?",
     "options": {
-      "alternativeA": "Na minha família, sou aquele que trabalha duro que se organiza e acaba ajudando os outros.",
-      "alternativeB": "Na minha família, sou o mais despreocupado, de vez em quando peço ajuda aos outros.",
-      "alternativeC": "Na minha família, sou o que vai dando um jeito nas contas, juntando aqui e ali pra passar pro mês seguinte.",
-      "alternativeD": "Na minha família, sou aquele que persegue seus sonhos, mesmo que pra isso seja preciso contar com a paciência dos outros.",
-      "alternativeE": "Na minha família, sou visto como racional com o dinheiro, o que consegue crescer mesmo quando os outros estão em crise."
+      "alternativeA": "Na minha família, sou aquele que trabalha duro que se organiza e ajuda os outros.",
+      "alternativeB": "Na minha família, sou o mais despreocupado. De vez em quando peço ajuda aos outros.",
+      "alternativeC": "Na minha família, sou o que vai dando um jeito nas contas, juntando aqui e ali para passar para o mês seguinte.",
+      "alternativeD": "Na minha família, sou aquele que persegue seus sonhos, mesmo que para isso seja preciso contar com a paciência dos outros.",
+      "alternativeE": "Na minha família, sou visto como racional com o dinheiro; o que consegue crescer mesmo quando os outros estão em crise."
     }
   },
   {
-    "questionText": "8 - Você pensa na sua aposentadoria?",
+    "questionText": "8 - Você pensa em sua aposentadoria?",
     "options": {
-      "alternativeA": "Meu dinheiro vem do meu esforço, no futuro também vai ser assim porque não me vejo parado, sem trabalhar.",
+      "alternativeA": "Meu dinheiro vem do meu esforço, no futuro também será assim, pois não me vejo parado, sem trabalhar.",
       "alternativeB": "Não consigo pensar em aposentadoria, só sei que quero estar muito feliz e continuar aproveitando a vida.",
-      "alternativeC": "Ando preocupado porque o presente já está difícil, mas tenho jogo de cintura pra me manter mês a mês.",
+      "alternativeC": "Ando preocupado porque o presente já está difícil, mas tenho jogo de cintura para me manter mês a mês.",
       "alternativeD": "Quando conseguir realizar meus sonhos, eles vão me garantir uma vida boa.",
       "alternativeE": "Estou planejando minha aposentadoria, porque tenho metas de chegar sossegado à velhice."
     }
   },
   {
-    "questionText": "9 - Qual destas frases melhor lhe define?",
+    "questionText": "9 - Qual dessas frases melhor te define?",
     "options": {
-      "alternativeA": "Sou muito disciplinado, gosto de ter sempre o controle da situação.",
-      "alternativeB": "Não sou muito preocupado, deixo a vida acontecer.",
-      "alternativeC": "Sou muito bom em me adaptar ao que o mundo me oferece vivo bem assim.",
-      "alternativeD": "Meus sonhos são construídos em cima dos valores que pra mim são importantes. ",
-      "alternativeE": "Sou movido por metas, estipulo para mim objetivos a serem cumpridos em um tempo determinado."
+      "alternativeA": "Sou muito disciplinado e gosto de ter sempre o controle da situação.",
+      "alternativeB": "Não sou muito preocupado. Deixo a vida acontecer.",
+      "alternativeC": "Sou muito bom em me adaptar ao que o mundo me oferece. Vivo bem assim.",
+      "alternativeD": "Meus sonhos são construídos em cima de valores que, para mim são importantes.",
+      "alternativeE": "Sou movido por metas. Estipulo, para mim objetivos a serem cumpridos em um tempo determinado."
     }
   },
   {
@@ -94,7 +94,7 @@ const array = [
     "options": {
       "alternativeA": "Saber economizar cada centavo.",
       "alternativeB": "Viver intensamente um dia de cada vez.",
-      "alternativeC": "Do meu jeitinho para garantir que as coisas deem certo.",
+      "alternativeC": "Dou meu jeitinho para garantir que as coisas deem certo.",
       "alternativeD": "Acreditar no futuro e investir tudo o que puder no meu projeto pessoal.",
       "alternativeE": "Sempre manter uma reserva financeira."
     }
@@ -114,18 +114,18 @@ const array = [
     "options": {
       "alternativeA": "Aplicaria uma parte no empreendimento que venho construindo e pouparia o restante.",
       "alternativeB": "Viveria os melhores dias da minha vida sem me preocupar com dinheiro.",
-      "alternativeC": "Quitaria minhas dividas e depois aproveitaria o restante para viajar e quem sabe comprar um carro.",
+      "alternativeC": "Quitaria minhas dividas. Depois, aproveitaria o restante para viajar e, quem sabe, comprar um carro.",
       "alternativeD": "Investiria em um projeto pessoal e garantiria um futuro feliz.",
       "alternativeE": "Estudaria o mercado financeiro e faria o dinheiro render mais."
     }
   },
   {
-    "questionText": "13 - Em uma linha do tempo como segue sua vida financeira?",
+    "questionText": "13 - Em uma linha do tempo, como segue a sua vida financeira?",
     "options": {
       "alternativeA": "Segue devagar e sempre um degrau por vez.",
       "alternativeB": "Cheia de altos e baixos, mas um dia chego lá.",
-      "alternativeC": "Com algumas dificuldades, porem mantendo o essencial.",
-      "alternativeD": "Segue com alguns recomeços, sem está pensando no futuro.",
+      "alternativeC": "Com algumas dificuldades, porém mantendo o essencial.",
+      "alternativeD": "Segue com alguns recomeços, sem estar pensando no futuro.",
       "alternativeE": "Em progressão continua muito bem planejada."
     }
   },
@@ -135,12 +135,12 @@ const array = [
       "alternativeA": "Com conquistas e investimentos diários.",
       "alternativeB": "Com as conquistas que surgem ao acaso.",
       "alternativeC": "Conto com os créditos.",
-      "alternativeD": "Com motivação.",
-      "alternativeE": "Com minha atenção para as oportunidades de investimento para longo prazo."
+      "alternativeD": "Conto com motivação.",
+      "alternativeE": "Conto com minha atenção para as oportunidades de investimento para longo prazo."
     }
   },
   {
-    "questionText": "15 - Como você planeja sua vida?",
+    "questionText": "15 - Como você planeja a sua vida?",
     "options": {
       "alternativeA": "Procuro ser organizado pra que não falte dinheiro no futuro.",
       "alternativeB": "Viver de planos não combina com meu estilo de vida.",
@@ -154,7 +154,7 @@ const array = [
     "options": {
       "alternativeA": "Optaria pelo emprego que possibilitasse conciliar os estudos e oferecesse maior garantia de estabilidade.",
       "alternativeB": "Escolheria o que pagasse mais, mesmo que fosse um emprego temporário.",
-      "alternativeC": "Colocaria curriculum em todos os lugares, o primeiro que chamasse eu já aceitaria. Afinal nunca se sabe o dia de amanhã.",
+      "alternativeC": "Colocaria curriculos em todos os lugares, O primeiro que chamasse eu aceitaria. Afinal, nunca se sabe o dia de amanhã.",
       "alternativeD": "Não me importo se o emprego demora para chegar o importante é que ele realize os meus sonhos.",
       "alternativeE": "Pesquisaria os benefícios de cada empresa e optaria pela que melhor pudesse progredir financeiramente."
     }
@@ -162,11 +162,11 @@ const array = [
   {
     "questionText": "17 - O que você faria em uma situação de falência?",
     "options": {
-      "alternativeA": "Buscaria nos aprendizados da minha caminhada uma solução e sairia aos poucos da falência.",
+      "alternativeA": "Buscaria, nos aprendizados da minha caminhada, uma solução a fim de sair, aos poucos, da falência.",
       "alternativeB": "Buscaria ajuda dos familiares porque preciso fazer as coisas que gosto.",
-      "alternativeC": "Parece que sempre estou em falência, todo mês é uma luta pra pagar as contas e sempre estou devendo algumas.",
+      "alternativeC": "Parece que sempre estou em falência. Todo mês é uma luta pra pagar as contas e sempre estou devendo algumas.",
       "alternativeD": "Ficaria um pouco sem rumo, preciso do dinheiro para realizar meus projetos.",
-      "alternativeE": "Não me imagino em uma situação destas porque tenho sempre o controle de toda situação."
+      "alternativeE": "Não me imagino em uma situação como essa, pois tenho sempre o controle da minha vida financeira."
     }
   },
   {
@@ -182,9 +182,9 @@ const array = [
   {
     "questionText": "19 - O que você faz quando o ano termina?",
     "options": {
-      "alternativeA": "Avalia o ano que passou e reorganiza as finanças.",
+      "alternativeA": "Avalio o ano que passou e reorganizo as finanças.",
       "alternativeB": "Aproveito pra comemorar e celebrar a vida, não é hora de pensar em dinheiro.",
-      "alternativeC": "Surpreendo-me todo ano como consigo sempre me articular com as contas.",
+      "alternativeC": "Surpreendo-me todo ano em relação a como consigo me articular com as contas.",
       "alternativeD": "É o momento que mais penso nos meus sonhos e imagino onde estarei em um ano.",
       "alternativeE": "Estudo os ganhos que tive e organizo formas de maximizar as finanças par ao ano seguinte."
     }
